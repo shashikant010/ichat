09bf20582bc3fee788eafd6b2e7d7e2664f5119e
shashikant010 <shashikantyadav0503@gmail.com>
2024-02-25 12:12:17 +0530

    made a chat app

---
+++ nodeServer/index.js
@@ -0,0 +1,26 @@
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: "http://127.0.0.1:5500",  // replace with your client's origin
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  });
+  
+  const users={};
+  
+  io.on('connection',socket =>{
+      socket.on('new-user-joined',name=>{
+          console.log('new user ',name)
+          users[socket.id]=name;
+          socket.broadcast.emit('user-joined', name)
+      })
+      socket.on('send',message=>{
+          socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+      })
+      socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',{name:users[socket.id],message:message});
+        delete users[socket.id];
+      })
+  })
+  
